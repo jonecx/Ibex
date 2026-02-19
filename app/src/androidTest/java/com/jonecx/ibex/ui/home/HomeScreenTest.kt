8242606418fd6004e04964ddae32ff445cce8121
Yonathan Redda <yredda@cisco.com>
2026-02-19 14:53:15 -0800

    Adds test for HomeScreen

---
+++ app/src/androidTest/java/com/jonecx/ibex/ui/home/HomeScreenTest.kt
@@ -0,0 +1,66 @@
+package com.jonecx.ibex.ui.home
+
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithText
+import com.jonecx.ibex.util.setIbexContent
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class HomeScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setup() {
+        composeTestRule.setIbexContent {
+            HomeScreen(onSourceSelected = {})
+        }
+    }
+
+    @Test
+    fun testHomeScreenDisplaysAppName() {
+        composeTestRule.onNodeWithText("Ibex").assertIsDisplayed()
+    }
+
+    @Test
+    fun testHomeScreenDisplaysLocalSection() {
+        composeTestRule.onNodeWithText("Local").assertIsDisplayed()
+    }
+
+    @Test
+    fun testHomeScreenDisplaysRemoteSection() {
+        composeTestRule.onNodeWithText("Remote").assertIsDisplayed()
+    }
+
+    @Test
+    fun testHomeScreenDisplaysAllLocalSourceTiles() {
+        composeTestRule.onNodeWithText("Storage").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Downloads").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Images").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Videos").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Audio").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Documents").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Apps").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Recent").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Analysis").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Trash").assertIsDisplayed()
+    }
+
+    @Test
+    fun testHomeScreenDisplaysAllRemoteSourceTiles() {
+        composeTestRule.onNodeWithText("Cloud").assertIsDisplayed()
+        composeTestRule.onNodeWithText("SMB/CIFS").assertIsDisplayed()
+        composeTestRule.onNodeWithText("FTP").assertIsDisplayed()
+    }
+
+    @Test
+    fun testHomeScreenDisplaysComingSoonForRemoteSources() {
+        composeTestRule.onAllNodesWithText("Coming soon", useUnmergedTree = true)
+            .assertCountEquals(3)
+    }
+}
